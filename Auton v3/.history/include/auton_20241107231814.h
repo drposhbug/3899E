3de7d62016255fb1bb7c1e90297e9e683a2b396c
drposhbug <57801917+Actmark@users.noreply.github.com>
2024-12-24 03:00:55 -0500

    fixing issue with slipThreshold

---
+++ Auton v3/.history/include/auton_20241107231814.h
@@ -0,0 +1,16 @@
+#ifndef AUTON_H
+#define AUTON_H
+
+//void testNormalizeAngle();
+//void driveForDistance(double distanceCm, double heading); 
+//void driveForDistancePID(double distanceCm, double heading); 
+
+void autonRoutine1();
+void autonRoutine2();
+void autonRoutine3();
+void autonRoutine4();
+void autonRoutine6();
+void autonRoutine7();
+void autonSkills(); 
+#endif // AUTON_H
+
