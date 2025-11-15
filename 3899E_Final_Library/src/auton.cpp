d057de6358155fd0a44dfb98ab91e3e432ae73fc
Actmark <57801917+Actmark@users.noreply.github.com>
2025-11-15 01:54:32 -0500

    auton copied over from PID branch

--- 3899E_Final_Library/src/auton.cpp
+++ 3899E_Final_Library/src/auton.cpp
@@ -15,7 +15,7 @@ using namespace vex; // Use the VEX namespace
 */                    
 
 //turnOdometry(turnAmount, breakDistance, minSpeed, maxSpeed)
-   void autonTest()
+   void autonLeft()
 {
     initializeOpticalSensor();
 
@@ -43,14 +43,18 @@ using namespace vex; // Use the VEX namespace
     
 
 
-    //george this is your stuff
-    forwardMP(63, 38, 180, 15, 0.815, 0.0, 0.0, 0.0, 0.55, 0.3, 80);
-    leftMP(90,60,8);
+    //george this is MY stuffnegnagaffgafgniafa
+    forwardMP(79, 38, 180, 20, 0.815, 0.0, 0.0, 0.0, 0.55, 0.3, 80);
+    wait(200, msec);
+    leftMP(90,60,12.5);
+    wait(200, msec);
     forwardMP(15, 10, 90, 15, 0.815, 0.0, 0.0, 0.0, 0.55, 0.3, 70);
-    wait(1000, msec);
+    wait(200, msec);
     backwardMP(20, 15, 90, 10, 0.815, 0.00, 0.00, 0., 0.55, 0.3, 50);
-    leftMP(180,134,15);
-    forwardMP(26, 18, 0, 15, 0.815, 0.0, 0.0, 0.0, 0, 0, 80);
+    wait(200, msec);
+    leftMP(175,125,20);
+    wait(200, msec);
+    forwardMP(35, 18, 0, 15, 0.815, 0.0, 0.0, 0.0, 0, 0, 80);
 
 
 
@@ -78,6 +82,30 @@ using namespace vex; // Use the VEX namespace
     */
 
     //wait(10000, msec);  // Wait to test resistance
+}
+
+void autonRight(){
+    initializeOpticalSensor();
+
+    InertialSensor.setRotation(0, degrees);
+ 
+    headingOffset = 0;
+    
+    forwardMP(80, 38, 0, 20, 0.815, 0.0, 0.0, 0.0, 0.55, 0.3, 80);
+    wait(200, msec);
+    rightMP(90,65,12.5);
+    wait(200, msec);
+    forwardMP(15, 10, 90, 15, 0.815, 0.0, 0.0, 0.0, 0.55, 0.3, 70);
+    wait(200, msec);
+    backwardMP(20, 15, 90, 10, 0.815, 0.00, 0.00, 0., 0.55, 0.3, 50);
+    wait(200, msec);
+    rightMP(78,50,20);
+    wait(200, msec);
+    backwardMP(3, 3, 90, 10, 0, 0.00, 0.00, 0., 0, 0, 20);
+    wait(200, msec);
+    rightMP(78,50,20);
+    wait(200, msec);
+    forwardMP(37, 18, 270, 15, 0.815, 0.0, 0.0, 0.0, 0, 0, 80);
 }
 
 
